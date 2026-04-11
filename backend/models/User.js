e8b1b3b26d7d3864d1f415af26a1932b3b1372bf
som62630 <271489315+som62630@users.noreply.github.com>
2026-04-11 14:03:59 +0530

    fix: remove next() from async mongoose hook to fix signup error

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,11 +8,11 @@ const userSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 });
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
+
 
 userSchema.methods.comparePassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
